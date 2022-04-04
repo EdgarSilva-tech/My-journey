aa26153cbebd5b222fc8f7942ce8c18e06488c64
Edgar Silva <81367614+EdgarSilva-tech@users.noreply.github.com>
2022-04-04 20:50:36 +0100

    Update Auction.sol

--- BlockChain/Auction.sol
+++ BlockChain/Auction.sol
@@ -3,23 +3,46 @@
 pragma solidity 0.8.11;
 
 contract Auction {
-    address public owner;
-    uint public initialBid;
-    mapping(address => uint) public HighestBidder;
-    uint public [] bids;
+    mapping (address => uint) public MyBid; 
+    uint public InitialBid;
+    uint index;
+    address payable public owner;
+    uint [] public bids;
 
-    constructor(uint firstbid) {
-        owner = msg.sender;
-        initialBid = firstbid;
+    constructor(uint _FirstBid) {
+        owner = payable(msg.sender);
+        InitialBid = _FirstBid;
+        index = 0;
+        bids.push(InitialBid);
     }
 
-    modifier BiggerThanLast(uint bids) {
-        require(bids > HighestBidder[msg.sender], "Please submit a bid higher than that of the highest bidder");
+    modifier HigherThanLast(uint _Bid) {
+        require(_Bid > bids[index], "Please make a bid higher than the last one");
         _;
     }
 
-    function bid(uint _bid) public payable BiggerThanLast(_bid) {
-        require(_bid > initialBid, "Please submit a bid higher than the starting point.");
-        HighestBidder[msg.sender];
+    modifier WinnerBid {
+        //require(msg.value == bids[index], "Only the winning bid of the auction can be submitted");
+        require(MyBid[msg.sender] == bids[index], "Only the winning address can call this function");
+        _;
+    }
+
+    modifier DeadLine {
+        require(block.timestamp < block.timestamp + 1 hours, "The auction is finished!");
+        _;
+    }
+
+    function Bid(uint _bid) public HigherThanLast(_bid) DeadLine {
+        MyBid[msg.sender] = _bid;
+        bids.push(_bid);
+        index++;
+    }
+
+    function currentBid() public view returns (uint) {
+        return bids[index];
+    }
+
+    function winner() payable external WinnerBid {
+        owner.transfer(msg.value);
     }
 }
